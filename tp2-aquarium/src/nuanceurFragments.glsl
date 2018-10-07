75002581d88d17a013532ae774a6c028b958b1bc
julienlegault96 <julien-3.legault@polymtl.ca>
2018-10-07 15:44:51 -0400

    partie 3 terminee

--- tp2-aquarium/src/nuanceurFragments.glsl
+++ tp2-aquarium/src/nuanceurFragments.glsl
@@ -1,5 +1,6 @@
 #version 410
 
+uniform int modeSelection;
 uniform int attEloignement;
 const float debAttenuation = 30.0;
 const float finAttenuation = 50.0;
@@ -16,18 +17,14 @@ void main( void )
    FragColor = AttribsIn.couleur;
 
    // atténuer selon la profondeur
-   if ( attEloignement == 1 )
+   if ( attEloignement == 1 && modeSelection == 0)
    {
-      // Mettre un énoncé bidon afin que l'optimisation du compilateur n'élimine l'attribut "attEloignement".
-      // Vous ENLEVEREZ ce test inutile!
-      if ( FragColor.x < -10000.0 ) discard;
-      
       // Obtenir la distance à la caméra du sommet dans le repère de la caméra
-      //float dist = ...;
+      float dist = gl_FragCoord.z / gl_FragCoord.w;
       // Obtenir un facteur d'interpolation entre 0 et 1
-      //float factDist = smoothstep( ... );
+      float factDist = smoothstep (  50 ,  30 ,  dist );
       // Modifier la couleur du fragment en utilisant ce facteur
-      // ...
+      FragColor = factDist*FragColor;      
 
       // pour déboguer et « voir » le facteur ou la distance, on peut utiliser:
       //FragColor = vec4( vec3(factDist), 1.0 );
--- tp2-aquarium/src/nuanceurSommets.glsl
+++ tp2-aquarium/src/nuanceurSommets.glsl
@@ -3,6 +3,7 @@
 const float M_PI = 3.14159265358979323846;	// pi
 const float M_PI_2 = 1.57079632679489661923;	// pi/2
 
+uniform int modeSelection;
 uniform mat4 matrModel;
 uniform mat4 matrVisu;
 uniform mat4 matrProj;
@@ -24,9 +25,19 @@ void main( void )
    // transformation standard du sommet
    gl_Position = matrProj * matrVisu * matrModel * Vertex;
 
+   // degrader de la couleur
+    vec4 bleu = vec4(0.0, 1.0, 1.0, 1.0);    
 
-   // couleur du sommet
-   AttribsOut.couleur = Color;
+    // couleur du sommet
+    if (modeSelection == 0) 
+    {
+        AttribsOut.couleur = mix(Color, bleu,  Vertex.z);
+    }
+    else 
+    {
+        AttribsOut.couleur = Color;
+    }
+
    gl_ClipDistance[0] = dot( planRayonsX, matrModel * Vertex );
    gl_ClipDistance[1] = dot( planRayonsX * -1.0f, matrModel * Vertex );
    gl_ClipDistance[2] = dot( planDragage, matrModel * Vertex );
