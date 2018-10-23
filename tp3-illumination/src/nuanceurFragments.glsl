01f6f3f2218e26744c0f74e032fc22623b0eec48
julienlegault96 <julien-3.legault@polymtl.ca>
2018-10-23 13:17:34 -0400

    Phong

--- tp3-illumination/src/nuanceurFragments.glsl
+++ tp3-illumination/src/nuanceurFragments.glsl
@@ -28,7 +28,7 @@ layout (std140) uniform MaterialParameters
 // Définition des paramètres globaux du modèle de lumière
 layout (std140) uniform LightModelParameters
 {
-   vec4 ambient;       // couleur ambiante
+   vec4 ambient;       // couleureur ambiante
    bool localViewer;   // observateur local ou à l'infini?
    bool twoSide;       // éclairage sur les deux côtés ou un seul?
 } LightModel;
@@ -39,19 +39,23 @@ layout (std140) uniform varsUnif
    int typeIllumination;     // 0:Gouraud, 1:Phong
    bool utiliseBlinn;        // indique si on veut utiliser modèle spéculaire de Blinn ou Phong
    bool utiliseDirect;       // indique si on utilise un spot style Direct3D ou OpenGL
-   bool afficheNormales;     // indique si on utilise les normales comme couleurs (utile pour le débogage)
+   bool afficheNormales;     // indique si on utilise les normales comme couleureurs (utile pour le débogage)
    // partie 3: texture
    int texnumero;            // numéro de la texture appliquée
-   bool utiliseCouleur;      // doit-on utiliser la couleur de base de l'objet en plus de celle de la texture?
+   bool utilisecouleureur;      // doit-on utiliser la couleureur de base de l'objet en plus de celle de la texture?
    int afficheTexelFonce;    // un texel noir doit-il être affiché 0:noir, 1:mi-coloré, 2:transparent?
 };
 
 uniform sampler2D laTexture;
+uniform mat4 matrModel;
+uniform mat4 matrVisu;
 
 /////////////////////////////////////////////////////////////////
 
 in Attribs {
    vec4 couleur;
+   vec3 normal;
+   vec3 pos;   
 } AttribsIn;
 
 out vec4 FragColor;
@@ -64,20 +68,52 @@ float calculerSpot( in vec3 D, in vec3 L )
 
 vec4 calculerReflexion( in vec3 L, in vec3 N, in vec3 O )
 {
-   vec4 grisUniforme = vec4(0.7,0.7,0.7,1.0);
-   return( grisUniforme );
+    // Phong
+   vec4 coul = vec4(0.0, 0.0, 0.0, 1.0);
+
+   // diffuse
+   float NdotL = max ( 0.0, dot( N, L ) );
+   coul += FrontMaterial.diffuse * LightSource.diffuse*NdotL;
+
+   // speculaire
+   if (utiliseBlinn)
+   {    
+    float NdotHV = max( 0.0, dot( normalize( L + O ), N ) );
+    coul += FrontMaterial.specular*LightSource.specular*pow(max(NdotHV, 0.0),FrontMaterial.shininess);    
+   }
+
+   else
+   {
+    float NdotHV = max( 0.0, dot( reflect( -L, N ), O ) );
+    coul += FrontMaterial.specular*LightSource.specular*pow(max(NdotHV, 0.0),FrontMaterial.shininess);
+   }
+    // ambient
+   coul += FrontMaterial.ambient * LightSource.ambient;
+
+   return coul;
 }
 
 void main( void )
 {
-   // ...
+  if(typeIllumination == 0)
+  {
+    FragColor = AttribsIn.couleur;
+  }
 
-   // assigner la couleur finale
-   //FragColor = AttribsIn.couleur;
-   FragColor = vec4( 0.5, 0.5, 0.5, 1.0 ); // gris moche!
+  else
+  {    
+   vec3 O = normalize(-AttribsIn.pos);
+   vec3 N = AttribsIn.normal;
 
-   // vec4 coul = calculerReflexion( L, N, O );
-   // ...
+   FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+  
+    for (int i=0; i<2;  i++)
+    {
+     vec3 L = normalize(vec3(matrVisu*LightSource.position[i]/LightSource.position[i].w).xyz - AttribsIn.pos);
+     FragColor += calculerReflexion(L, N, O); 
+    }
 
-   //if ( afficheNormales ) FragColor = vec4(N,1.0);
+   FragColor += FrontMaterial.emission + FrontMaterial.ambient * LightModel.ambient;
+   FragColor = clamp( FragColor, 0.0, 1.0 );
+  }
 }
